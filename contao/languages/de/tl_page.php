d6a282dde865298788d42716cfb49dfed3ba0a6b
Malte Müns <m.muens@muensmedia.de>
2022-05-25 15:40:59 +0200

    [TASK] Change wording to API-Schlüssel

--- contao/languages/de/tl_page.php
+++ contao/languages/de/tl_page.php
@@ -11,6 +11,6 @@ declare(strict_types=1);
  */
 
 $GLOBALS['TL_LANG']['tl_page']['erecht24_legend'] = 'eRecht24';
-$GLOBALS['TL_LANG']['tl_page']['er24ApiKey'] = ['eRecht24 API-Key', 'Der API Key des eRecht24 Projekts.'];
+$GLOBALS['TL_LANG']['tl_page']['er24ApiKey'] = ['eRecht24 API-Schlüssel', 'Der API-Schlüssel des eRecht24 Projekts.'];
 $GLOBALS['TL_LANG']['tl_page']['er24Secret'] = ['eRecht24 Secret', 'Das Secret für den Push-Client der eRecht24 API.'];
 $GLOBALS['TL_LANG']['tl_page']['er24ClientId'] = ['eRecht24 Client-ID', 'Die Client-ID des Push-Client der eRecht24 API.'];
